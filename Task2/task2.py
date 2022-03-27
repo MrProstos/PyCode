3e7425cc0e4a25ae03f56953f0679622e41c7b86
Vladislaw Mikhin <vladmihin28@gmail.com>
2022-03-27 07:17:48 +0000

    2 задание почти готово

--- Task2/task2.py
+++ Task2/task2.py
@@ -11,31 +11,43 @@ class MyHandler(PatternMatchingEventHandler):
         with open("%s" % os.path.normpath(path_log_file + "/log.txt"), "a") as file:
             file.write("%s %s %s\n" %
                        (time.asctime(), event.src_path, event.event_type))
+
         print(time.asctime(), event.src_path, event.event_type)
-        
+
+    def mod_path(self, event):
+        mod_file = str(event.src_path).partition(
+            os.path.basename("/"+os.path.dirname(path_dir)))[1:]
+
+        return path_replic_dir + mod_file[0] + mod_file[1]
 
     def on_modified(self, event):
         self.process(event)
-        print(str(event.src_path).replace())
+        if not event.is_directory:
+            os.system("cp %s %s" % (event.src_path, self.mod_path(event)))
 
     def on_created(self, event):
         self.process(event)
+        if event.is_directory:
+            os.system("cp -r %s %s" %( event.src_path, self.mod_path(event)))
+        else:
+            os.system("cp %s %s" %( event.src_path, self.mod_path(event)))
 
     def on_deleted(self, event):
         self.process(event)
+        os.system("rm %s" % self.mod_path(event))
 
 
 if __name__ == '__main__':
 
     try:
-        script, path_dir, path_replic_dir, path_log_file, time_interval = sys.argv
+        _, path_dir, path_replic_dir, path_log_file, time_interval = sys.argv
 
         if os.path.exists(path_replic_dir):
             os.system("cp -r %s %s" % (path_dir, path_replic_dir))
-            
 
         observer = Observer()
-        observer.schedule(MyHandler(), path=path_dir, recursive=True)
+        observer.schedule(MyHandler(), path=path_dir,
+                          recursive=True)
         observer.start()
 
         try:
