b5450fcc3953d76ed8b5a7dc88a357e036f1b56f
Vladislaw Mikhin <vladmihin28@gmail.com>
2022-03-28 03:11:02 +0000

    сокет на двух портах

--- Task3/Server.py
+++ Task3/Server.py
@@ -1,38 +1,79 @@
-
-import uuid
 import socket
-import sys
+import threading
+import uuid
 
-# создаемTCP/IP сокет
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Привязываем сокет к порту
-server_address = ("localhost", 8000)
-print("Старт сервера на %s порт %s " % server_address)
-sock.bind(server_address)
+class socket_server():
 
-# Слушаем входящие подключения
-sock.listen(50)
+    def __init__(self, ip, port):
+        self.ip = ip
+        self.port = port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.bind((self.ip, self.port))
+        self.sock.listen(50)
 
-while True:
-    # ждем соединения
-    print("Ожидание соединения...")
-    connection, client_address = sock.accept()
-    try:
-        print("Подключено к:", client_address)
-        # Принимаем данные порциями и ретранслируем их
+    def server1_run(self):
+
         while True:
-            data = connection.recv(1024)
-            print(f"Получено: {data.decode()}")
-            if data:
-                print("Обработка данных...")
-                data = str(uuid.uuid4()).encode()
-                print("Отправка обратно клиенту.")
-                connection.sendall(data)
-            else:
-                print("Нет данных от:", client_address)
-                break
+            # ждем соединения
+            print("Ожидание соединения сервер 1...")
 
-    finally:
-        # Очищаем соединение
-        connection.close()
+            connection, client_address = self.sock.accept()
+            try:
+                print("Подключено к:", client_address)
+                # Принимаем данные порциями и ретранслируем их
+                while True:
+                    identifier = connection.recv(1024)
+                    print("Получено: %s" % identifier.decode())
+                    if identifier:
+                        print("Обработка данных...")
+                        msg = str(uuid.uuid4()).encode()
+                        a = {"%s" % identifier.decode(): "%s" % msg.decode()}
+                        unique_data.update(a)
+                        print("Отправка обратно клиенту.")
+
+                        connection.sendall(msg)
+                        connection.sendall(str(a).encode())
+
+                        print(unique_data)
+                    else:
+                        print("Нет данных от:", client_address)
+                        break
+
+            finally:
+                # Очищаем соединение
+                connection.close()
+
+    def server2_run(self):
+
+        while True:
+
+            # ждем соединения
+            print("Ожидание соединения сервер 2...")
+            connection, client_address = self.sock.accept()
+            try:
+                print("Подключено к:", client_address)
+                # Принимаем данные порциями и ретранслируем и
+
+            finally:
+                # Очищаем соединение
+                connection.close()
+
+
+if __name__ == '__main__':
+
+    unique_data = {}
+
+    ports = [8000, 8001]
+
+    for port in ports:
+
+        s = socket_server("localhost", port)
+
+        if port == 8000:
+            thread1 = threading.Thread(target=s.server1_run())
+            thread1.start()
+
+        if port == 8001:
+            thread2 = threading.Thread(target=s.server2_run())
+            thread2.start()
